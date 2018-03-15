33ad84cf38c36808822ee6f7cd72d617959c2f83
EC2 Default User <ec2-user@ip-172-31-5-37.us-east-2.compute.internal>
2018-03-15 09:01:39 +0000

    have

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   has_many :wants
-  has_many :want_user, through: :wants, class_name: "User", source: :user
+  has_many :want_users, through: :wants, class_name: "User", source: :user
+  has_many :haves, class_name: "Have"
+  has_many :have_users, through: :haves, class_name: "User", source: :user
   
   validates :code, presence: true, length: { maximum: 255 }
   validates :name, presence: true, length: { maximum: 255 }
